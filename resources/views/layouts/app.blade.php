fc2aed59892e9895f6d6415ceb0b9c59f8c5a445
Abdulwahab <disu.abdulwahab@gmail.com>
2023-12-25 10:53:01 +0100

    minor updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,10 +56,7 @@
                         <a href="{{ route('home') }}" class="nav-item nav-link active">Home</a>
                         <a href="about.html" class="nav-item nav-link">About</a>
                         <a href="service.html" class="nav-item nav-link">Service</a>
-                        <a href="menu.html" class="nav-item nav-link">Menu</a>
-                        
-
-                      
+                        <a href="menu.html" class="nav-item nav-link">Menu</a>                      
                         <a href="contact.html" class="nav-item nav-link">Contact</a>
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ms-auto">
